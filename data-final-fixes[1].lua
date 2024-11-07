6e6851696842f6e2bdc8ad3669ec14cfaad688cd
EMeindertsma <156250481+EMeindertsma@users.noreply.github.com>
2024-11-07 21:57:18 +0100

    Uploaded mod to Github
    Uploaded mod to Github for easier collaboration with multiple people. The [1]'s are an aretfact that I'll remove soon.

---
+++ data-final-fixes[1].lua
@@ -0,0 +1,10 @@
+if mods["boblogistics"] then
+	require('prototypes.bob-belts-update')
+end
+if mods["space-exploration"] then
+	require('prototypes.space-belts-update')
+end
+if mods["Krastorio2"] then
+	require("prototypes.superior-linked-belt")
+	require("prototypes.advanced-linked-belt")
+end

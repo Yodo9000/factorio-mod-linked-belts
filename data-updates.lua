df4646fd18f609b124ca3bfdb4d3f73587585f88
EMeindertsma <edo.meindertsma@gmail.com>
2024-11-14 15:45:13 +0100

    made linked belt entities yield the correct item when destroyed

--- data-updates.lua
+++ data-updates.lua
@@ -4,7 +4,8 @@ for _, prototype in pairs(data.raw["underground-belt"]) do
   local prototype_copy = util.table.deepcopy(prototype)
   prototype_copy.type = "linked-belt"
   prototype_copy.name = "linked-"..prototype.name
-  prototype_copy.hidden_in_factoriopedia = true -- need to change description etc if visible in Factoriopeida
+  prototype_copy.hidden_in_factoriopedia = true -- I'm not sure if this works. Need to change description etc if visible in Factoriopedia
+  prototype_copy.minable.result = prototype_copy.name
   prototype_copy.localised_name = {"entity-name.linked-belts", {"entity-name."..prototype.name}} -- has extra capitalisation
   for _, sprite_4_way in pairs(prototype_copy.structure) do -- should maybe be a bit more general to deal with differently defined sprites
     sprite_4_way.sheet.tint = tint
@@ -37,10 +38,12 @@ for _, prototype in pairs(data.raw["underground-belt"]) do
   }
 
   data:extend{prototype_copy, item, recipe}
+
+  -- Add recipe unlock to the correct technology:
   for _, technology in pairs(data.raw["technology"]) do
     for _, modifier in pairs(technology.effects or {}) do -- some technologies don't have effects
       if modifier.type == "unlock-recipe" then
-        if modifier.recipe == prototype.name then -- what if the original recipe has a different name!?
+        if modifier.recipe == prototype.name then -- doesn't work if the original recipe has a different name
           table.insert(technology.effects, {type = "unlock-recipe", recipe = prototype_copy.name})
           break
         end
